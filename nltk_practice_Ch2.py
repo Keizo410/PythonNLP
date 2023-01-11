785849d4d39f380c2f5f0723c318810a1e72f07f
Keizo Kato <iek427pop@outlook.jp>
2023-01-11 12:33:59 -0800

    Ch2 is introduced

---
+++ nltk_practice_Ch2.py
@@ -0,0 +1,18 @@
+#set up nltk library and 25000 free electronic books
+import nltk
+from nltk.corpus import gutenberg
+#set the book set as emma
+emma = gutenberg.words('austen-emma.txt')
+len(emma)
+
+#since books from corpus is different from book, we need to preprocess
+emma = nltk.Text(gutenberg.words('austen-emma.txt'))
+emma.concordance("surprize")
+
+#short example to display some info about the books
+for fileid in gutenberg.fileids():
+    num_charts = len(gutenberg.raw(fileid))
+    num_words = len(gutenberg.words(fileid))
+    num_sents = len(gutenberg.sents(fileid))
+    num_vocab = len(set(w.lower() for w in gutenberg.words(fileid)))
+    print(round(num_charts/num_words),round(num_words/num_sents),round(num_words/num_vocab),fileid)

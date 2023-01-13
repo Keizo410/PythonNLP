ef789bfe2d349574c06ee056ab50d2baf72ea3bf
Keizo Kato <iek427pop@outlook.jp>
2023-01-13 10:53:34 -0800

    Heavy chapter

--- nltk_practice_Ch2.py
+++ nltk_practice_Ch2.py
@@ -17,4 +17,16 @@ for fileid in gutenberg.fileids():
     num_vocab = len(set(w.lower() for w in gutenberg.words(fileid)))
     print(round(num_charts/num_words),round(num_words/num_sents),round(num_words/num_vocab),fileid)
 
-#
+# linguistic inquiry (stylistics)
+from nltk.corpus import brown
+news_text = brown.words(categories='news')
+fdist = nltk.FreqDist(w.lower() for w in news_text)
+modals = ['can', 'could', 'may', 'might','will']
+for m in modals:
+    print(m + ':', fdist[m], end = ' ')
+
+# compare several words distribution frequencies on several materials in matrix
+cfd = nltk.ConditionalFreqDist((genre,word) for genre in brown.categories() for word in brown.words(categories=genre))
+genres = ['news','religion','hobbies','science_fiction','romance','humor']
+modals = ['can','could','may','might','must','will']
+cfd.tabulate(conditions=genres,samples=modals)
